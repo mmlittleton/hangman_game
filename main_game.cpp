481668ea3c883eb8c9760fcc1a28926b5d5a479d
walkk11 <walkk11@my.erau.edu>
2026-03-05 14:05:28 -0700

    Initialize Hangman game with difficulty selection
    Added welcome message and difficulty options for Hangman game.

---
+++ main_game.cpp
@@ -0,0 +1,23 @@
+/*
+    Authors: Madeline Littleton and Kylee Walker
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string d;
+    cout << "Welcome to Hangman!" << endl;
+    
+    cout << "Difficulties:" << endl;
+    cout << "Easy" << endl;
+    cout << "Medium" << endl;
+    cout << "Hard" << endl;
+    cout << "Extra Hard" << endl;
+    
+    cout << "What difficulty would you like? ";
+    getline(cin, d);
+    
+}
