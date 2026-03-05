54e7d6a33d47d8bfc69f22572c08948072d7b7bd
walkk11 <walkk11@my.erau.edu>
2026-03-05 14:21:43 -0700

    Update main_game.cpp

--- main_game.cpp
+++ main_game.cpp
@@ -10,15 +10,16 @@ int main()
 {
     string d;
     cout << "Welcome to Hangman!" << endl;
+    while(d != exit) {
+        cout << "Difficulties:" << endl;
+        cout << "Easy" << endl;
+        cout << "Medium" << endl;
+        cout << "Hard" << endl;
+        cout << "Extra Hard" << endl;
+        cout << "Exit" << endl;
     
-    cout << "Difficulties:" << endl;
-    cout << "Easy" << endl;
-    cout << "Medium" << endl;
-    cout << "Hard" << endl;
-    cout << "Extra Hard" << endl;
-    
-    cout << "What difficulty would you like? ";
-    getline(cin, d);
-
+        cout << "What difficulty would you like? ";
+        getline(cin, d);
+    }
     return 0;
 }
