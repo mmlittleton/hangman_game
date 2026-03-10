68b332f46ab5e4a861971fd74e74a1e93cde8ff4
mmlittleton <mmlittleton421@gmail.com>
2026-03-10 14:13:39 -0700

    Initialize random seed and include headers
    Added necessary includes and srand initialization for difficulty functions.

--- difficulties.h
+++ difficulties.h
@@ -1,14 +1,19 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 string extra_hard(){
-  
+  srand(time(0));
+  ifstream fin("extra_hard.txt");
+  fin>>line
 }
 string hard(){
-  
+  srand(time(0));
 }
 string medium(){
-  
+  srand(time(0));
 }
 string easy(){
-  
+  srand(time(0));
 }
