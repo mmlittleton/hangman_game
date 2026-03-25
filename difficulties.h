3543b2739455c0b5e85f0541f29729d951403f80
mmlittleton <mmlittleton421@gmail.com>
2026-03-25 11:36:57 -0700

    Random number generation+file reading
    Program can open files based on user's difficulty selection, and the program will be able to pick a random word from the file based on random number generation and have each word assigned to a file.

--- difficulties.h
+++ difficulties.h
@@ -3,17 +3,64 @@
 #include <ctime>
 
 using namespace std;
+string word
 string extra_hard(){
   srand(time(0));
-  ifstream fin("extra_hard.txt");
-  fin>>line
+    std::ifstream file("extra_hard.txt");
+    std::string line;
+    int targetLine = rand() % 6) + 1; // The specific line you want to read
+    int currentLine = 0;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            currentLine++;
+            word=line;
+        }
+        file.close();
+    }
 }
 string hard(){
   srand(time(0));
+    std::ifstream file("hard.txt");
+    std::string line;
+    int targetLine = rand() % 6) + 1; // The specific line you want to read
+    int currentLine = 0;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            currentLine++;
+            word=line;
+        }
+        file.close();
+    }
 }
 string medium(){
-  srand(time(0));
+    srand(time(0));
+    std::ifstream file("medium.txt");
+    std::string line;
+    int targetLine = rand() % 6) + 1; // The specific line you want to read
+    int currentLine = 0;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            currentLine++;
+            word=line;
+        }
+        file.close();
+    }
 }
 string easy(){
-  srand(time(0));
+    srand(time(0));
+    std::ifstream file("easy.txt");
+    std::string line;
+    int targetLine = rand() % 6) + 1; // The specific line you want to read
+    int currentLine = 0;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            currentLine++;
+            word=line;
+        }
+        file.close();
+    };
 }
