2a96888ead1e17aefb1eb7cec910902e85e32925
walkk11 <walkk11@my.erau.edu>
2026-03-26 12:46:19 -0700

    Refactor single player difficulty selection logic

--- main_game.cpp
+++ main_game.cpp
@@ -10,7 +10,6 @@ using namespace std;
 
 int main()
 {
-    string d;
     string m;
     cout << "Welcome to Hangman!" << endl;
     
@@ -21,37 +20,45 @@ int main()
         m[i] = tolower(m[i]);
     }
     
-    while(d != "exit") {
-        cout << "Difficulties:" << endl;
-        cout << "Easy" << endl;
-        cout << "Medium" << endl;
-        cout << "Hard" << endl;
-        cout << "Extra Hard" << endl;
-        cout << "Exit" << endl;
+    if (m == "single") {
+        string d;
+        while(d != "exit") {
+            cout << "Difficulties:" << endl;
+            cout << "Easy" << endl;
+            cout << "Medium" << endl;
+            cout << "Hard" << endl;
+            cout << "Extra Hard" << endl;
+            cout << "Exit" << endl;
     
-        cout << "What difficulty would you like? ";
-        getline(cin, d);
+            cout << "What difficulty would you like? ";
+            getline(cin, d);
         
-        for(int i = 0; i < d.size(); i++) {
-            d[i] = tolower(d[i]);
-        }
+            for(int i = 0; i < d.size(); i++) {
+                d[i] = tolower(d[i]);
+            }
     
-        if(d == "easy") {
-            easyMode();
+            if(d == "easy") {
+                easyMode();
+            }
+            else if(d == "medium") {
+                mediumMode();
+            }
+            else if(d == "hard") {
+                hardMode();
+            }
+            else if(d == "extra hard") {
+                extraHardMode();
+            }
+            else if(d != "exit") {
+                cout << "Invalid difficulty. Try again." << endl;
+            }
         }
-        else if(d == "medium") {
-            mediumMode();
-        }
-        else if(d == "hard") {
-            hardMode();
-        }
-        else if(d == "extra hard") {
-            extraHardMode();
-        }
-        else if(d != "exit") {
-            cout << "Invalid difficulty. Try again." << endl;
-        }
-        
+    }
+    else if (m == "multiplayer") {
+        multiPlayerMode();
+    }
+    else {
+        cout << "Invalid mode." << endl;
     }
     return 0;
 }
