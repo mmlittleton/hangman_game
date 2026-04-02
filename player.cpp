20707414391622444383ee2c54f1a3292c253b56
walkk11 <walkk11@my.erau.edu>
2026-04-02 08:19:41 -0700

    Implement loss tracking in Player class
    Added methods to track losses and updated output format.

--- player.cpp
+++ player.cpp
@@ -1,3 +1,7 @@
+/*
+    Authors: Madeline Littleton and Kylee Walker
+*/
+
 #include "player.h"
 
 using namespace std;
@@ -5,11 +9,13 @@ using namespace std;
 Player::Player() {
     name = "Player";
     wins = 0;
+    losses = 0;
 }
 
 Player::Player(string n) {
     name = n;
     wins = 0;
+    losses = 0;
 }
 
 Player::~Player() {
@@ -23,7 +29,28 @@ int Player::getWins() const {
     return wins;
 }
 
+void Player::addLoss() {
+    losses++;
+}
+
+int Player::getLosses() const {
+    return losses;
+}
+
+string Player::getName() const {
+    return name;
+}
+
 ostream& operator<<(ostream& os, const Player& p) {
-    os << p.name << " wins: " << p.wins;
+    os << p.name << " | Wins: " << p.wins << " | Losses: " << p.losses;
     return os;
+}
+
+HumanPlayer::HumanPlayer() : Player("Player") {
+}
+
+HumanPlayer::HumanPlayer(string n) : Player(n) {
+}
+
+HumanPlayer::~HumanPlayer() {
 }
